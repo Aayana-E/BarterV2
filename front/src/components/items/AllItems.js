67982a4515858280fff3d26496785088cc317610
Rosalie Newman <rosalienewman0@gmail.com>
2023-05-28 10:16:25 -0400

    buttons are styled nicer

--- front/src/components/items/AllItems.js
+++ front/src/components/items/AllItems.js
@@ -3,6 +3,40 @@ import { collection, getDocs } from "firebase/firestore";
 import { db } from "../firebase/firebase-config";
 
 export default function AllItems({ items }) {
+  const messageButton = {
+    border: "none",
+    outline: "none",
+    color: "whitesmoke",
+    backgroundColor: "#609966",
+    fontFamily: "san-serif",
+    padding: "10px 20px",
+    fontSize: "14px",
+    fontWeight: "500",
+    borderRadius: "5px",
+    transition: "all ease 0.1s",
+    boxShadow: `0px 5px 0px 0px #9DC08B`,
+    width: `120px`,
+    textDecoration: "none",
+    textAlign: "center",
+    cursor: "pointer",
+  };
+  const detailsButton = {
+    border: "none",
+    outline: "none",
+    backgroundColor: "whitesmoke",
+    fontFamily: "san-serif",
+    padding: "10px 20px",
+    fontSize: "14px",
+    fontWeight: "500",
+    color: "black",
+    borderRadius: "5px",
+    transition: "all ease 0.1s",
+    boxShadow: `0px 5px 0px 0px #9DC08B`,
+    width: `120px`,
+    textDecoration: "none",
+    textAlign: "center",
+    cursor: "pointer",
+  };
   return (
     <div>
       <h2>Want these items?</h2>
@@ -13,20 +47,20 @@ export default function AllItems({ items }) {
           <div
             key={item.id}
             style={{
-              width: "500px",
+              width: "600px",
               display: "flex",
-              justifyContent: "center",
+              justifyContent: "right",
               gap: "2rem",
               border: "1px solid grey",
               padding: "2rem",
+              borderRadius: "10px",
+              margin: "1rem",
             }}
           >
             <div>{item.itemName}</div>
             <div style={{ fontStyle: "italic" }}>{item.category}</div>
-            <button>More Details</button>
-            <button style={{ color: "white", backgroundColor: "green" }}>
-              Request Trade
-            </button>
+            <button style={detailsButton}>More Details</button>
+            <button style={messageButton}>Request Trade</button>
           </div>
         ))}
       </div>
